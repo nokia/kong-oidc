9f60999207dd3d255ffc8a5a7c79c0038f4e07f5
phirvone <pekka.hirvonen@nokia.com>
2017-10-16 14:04:13 +0300

    First draft of unit test hierarchy (BaseCase -> MockableCase -> actual cases)

--- test/unit/test_filter.lua
+++ test/unit/test_filter.lua
@@ -1,47 +1,51 @@
 local filter = require("kong.plugins.oidc.filter")
 local lu = require("luaunit")
 
-TestFilter = {}
-ngx = {
+TestFilter = require("test.unit.base_case"):extend()
+
+function TestFilter:setUp()
+  TestFilter.super:setUp()
+  _G.ngx = {
     var = {
-        uri = ""
+      uri = ""
     }
-}
+  }
+end
+
+function TestFilter:tearDown()
+  TestFilter.super:tearDown()
+end
 
 local config =  {
-    filters = {  "^/pattern1$","^/pattern2$"}
+  filters = {  "^/pattern1$","^/pattern2$"}
 }
-function TestFilter:testIgnoreRequestWhenMatchingPattern1()
-    ngx.var.uri = "/pattern1"
-    lu.assertFalse(filter.shouldProcessRequest(config))
-end
 
+function TestFilter:testIgnoreRequestWhenMatchingPattern1()
+  ngx.var.uri = "/pattern1"
+  lu.assertFalse(filter.shouldProcessRequest(config))
+end
 
 function TestFilter:testIgnoreRequestWhenMatchingPattern2()
-    ngx.var.uri = "/pattern2"
-    lu.assertFalse(filter.shouldProcessRequest(config))
+  ngx.var.uri = "/pattern2"
+  lu.assertFalse(filter.shouldProcessRequest(config))
 end
-
 
 function TestFilter:testProcesseRequestWhenNoMatch()
-    ngx.var.uri = "/not_matching"
-    lu.assertTrue(filter.shouldProcessRequest(config))
+  ngx.var.uri = "/not_matching"
+  lu.assertTrue(filter.shouldProcessRequest(config))
 end
-
 
 function TestFilter:testProcessRequestWhenTheyAreNoFiltersNil()
-    ngx.var.uri = "/pattern1"
-    config.filters= nil
-    lu.assertTrue(filter.shouldProcessRequest(config))
+  ngx.var.uri = "/pattern1"
+  config.filters= nil
+  lu.assertTrue(filter.shouldProcessRequest(config))
 end
-
 
 function TestFilter:testProcessRequestWhenTheyAreNoFiltersEmpty()
-    ngx.var.uri = "/pattern1"
-    config.filters= {}
-    lu.assertTrue(filter.shouldProcessRequest(config))
+  ngx.var.uri = "/pattern1"
+  config.filters= {}
+  lu.assertTrue(filter.shouldProcessRequest(config))
 end
 
+
 lu.run()
-
-
