8c780aae087093dcfd71562387c3fcdbd2262add
Trojan295 <trojan295@gmail.com>
2018-06-10 18:15:22 +0200

    Improve tests

--- ci/run.sh
+++ ci/run.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-
-export LUA_VERSION=${LUA_VERSION:-5.1}
-export KONG_VERSION=${KONG_VERSION:-0.11.2-0}
-export LUA_RESTY_OPENIDC_VERSION=${LUA_RESTY_OPENIDC_VERSION:-1.5.3}
-
-apt-get update
-apt-get install -y unzip
-
-pip install hererocks
-hererocks lua_install -r^ --lua=${LUA_VERSION}
-export PATH=${PATH}:${PWD}/lua_install/bin
-
-luarocks install kong ${KONG_VERSION}
-luarocks install lua-resty-openidc ${LUA_RESTY_OPENIDC_VERSION}
-luarocks install lua-cjson
-luarocks install luaunit
-luarocks install luacov
+set -e
 
 lua -lluacov test/unit/test_filter.lua -o TAP --failure
 lua -lluacov test/unit/test_filters_advanced.lua -o TAP --failure
 lua -lluacov test/unit/test_utils.lua -o TAP --failure
-lua -lluacov test/unit/test_handler_mocking_openidc.lua --failure
+lua -lluacov test/unit/test_handler_mocking_openidc.lua -o TAP --failure
 lua -lluacov test/unit/test_introspect.lua -o TAP --failure
 lua -lluacov test/unit/test_utils_bearer_access_token.lua -o TAP --failure
+
---
+++ ci/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+export LUA_VERSION=${LUA_VERSION:-5.1}
+export KONG_VERSION=${KONG_VERSION:-0.13.1-0}
+export LUA_RESTY_OPENIDC_VERSION=${LUA_RESTY_OPENIDC_VERSION:-1.6.0}
+
+pip install hererocks
+hererocks lua_install -r^ --lua=${LUA_VERSION}
+export PATH=${PATH}:${PWD}/lua_install/bin
+
+luarocks install kong ${KONG_VERSION}
+luarocks install lua-resty-openidc ${LUA_RESTY_OPENIDC_VERSION}
+luarocks install lua-cjson
+luarocks install luaunit
+luarocks install luacov
+
