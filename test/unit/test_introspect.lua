f6ae7658b1b6c87fcb7ec50ed76838cdb618ca23
Tom Milligan <tommilligan@users.noreply.github.com>
2018-08-02 14:42:53 +0100

    Add X-Userinfo header on introspection success as well

--- test/unit/test_introspect.lua
+++ test/unit/test_introspect.lua
@@ -18,16 +18,32 @@ function TestIntrospect:test_access_token_exists()
   function dict:get(key) return key end
   _G.ngx.shared = {introspection = dict }
 
+  ngx.encode_base64 = function(x)
+    return "eyJzdWIiOiJzdWIifQ=="
+  end
+
+  local headers = {}
+  ngx.req.set_header = function(h, v)
+    headers[h] = v
+  end
+
   self.handler:access({introspection_endpoint = "x"})
   lu.assertTrue(self:log_contains("introspect succeeded"))
+  lu.assertEquals(headers['X-Userinfo'], "eyJzdWIiOiJzdWIifQ==")
 end
 
 function TestIntrospect:test_no_authorization_header()
   package.loaded["resty.openidc"].authenticate = function(...) return {}, nil end
   ngx.req.get_headers = function() return {} end
 
+  local headers = {}
+  ngx.req.set_header = function(h, v)
+    headers[h] = v
+  end
+
   self.handler:access({introspection_endpoint = "x"})
   lu.assertFalse(self:log_contains(self.mocked_ngx.ERR))
+  lu.assertEquals(headers['X-Userinfo'], nil)
 end
 
 
