0709abf5c76be2fe31e1e36b5487e0073773deb3
emoj <Emo_Jacob@bah.com>
2019-05-15 11:30:07 -0400

    updating to allow multi-authentication and working against kong 1.X+

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -4,12 +4,63 @@ local utils = require("kong.plugins.oidc.utils")
 local filter = require("kong.plugins.oidc.filter")
 local session = require("kong.plugins.oidc.session")
 
-local singletons = require "kong.singletons"
 local constants = require "kong.constants"
-local responses = require "kong.tools.responses"
 
-OidcHandler.PRIORITY = 1000
+local kong = kong
 
+OidcHandler.PRIORITY = 1002
+
+local function internal_server_error(err)
+  kong.log.err(err)
+  return kong.response.exit(500, { message = "An unexpected error occurred" })
+end
+
+local function set_consumer(consumer, credential, token)
+  local set_header = kong.service.request.set_header
+  local clear_header = kong.service.request.clear_header
+
+  if consumer and consumer.id then
+    set_header(constants.HEADERS.CONSUMER_ID, consumer.id)
+  else
+    clear_header(constants.HEADERS.CONSUMER_ID)
+  end
+
+  if consumer and consumer.custom_id then
+    set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, consumer.custom_id)
+  else
+    clear_header(constants.HEADERS.CONSUMER_CUSTOM_ID)
+  end
+
+  if consumer and consumer.username then
+    set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
+  else
+    clear_header(constants.HEADERS.CONSUMER_USERNAME)
+  end
+
+  kong.client.authenticate(consumer, credential)
+
+  if credential then
+    if token.scope then
+      set_header("x-authenticated-scope", token.scope)
+    else
+      clear_header("x-authenticated-scope")
+    end
+
+    if token.authenticated_userid then
+      set_header("x-authenticated-userid", token.authenticated_userid)
+    else
+      clear_header("x-authenticated-userid")
+    end
+
+    clear_header(constants.HEADERS.ANONYMOUS) -- in case of auth plugins concatenation
+
+  else
+    set_header(constants.HEADERS.ANONYMOUS, true)
+    clear_header("x-authenticated-scope")
+    clear_header("x-authenticated-userid")
+  end
+
+end
 
 function OidcHandler:new()
   OidcHandler.super.new(self, "oidc")
@@ -18,7 +69,7 @@ end
 function OidcHandler:access(config)
   OidcHandler.super.access(self)
 
-  if ngx.ctx.authenticated_credential and config.anonymous ~= "" then
+  if config.anonymous and kong.client.get_credential() then
     -- we're already authenticated, and we're configured for using anonymous,
     -- hence we're in a logical OR between auth methods and we're already done.
     return
@@ -49,6 +100,10 @@ function handle(oidcConfig)
     response = make_oidc(oidcConfig)
     if response then
       if (response.user) then
+        local tmp_user = response.user
+        tmp_user.id = response.user.sub
+        tmp_user.username = response.user.preferred_username
+        set_consumer(tmp_user, nil, nil)
         utils.injectUser(response.user)
       end
       if (response.access_token) then
@@ -69,19 +124,20 @@ function make_oidc(oidcConfig)
       ngx.log(ngx.DEBUG, "Entering recovery page: " .. oidcConfig.recovery_page_path)
       ngx.redirect(oidcConfig.recovery_page_path)
     end
-    if oidcConfig.anonymous ~= "" then
+    if oidcConfig.anonymous then
       -- get anonymous user
-      local consumer_cache_key = singletons.db.consumers:cache_key(oidcConfig.anonymous)
-      local consumer, err      = singletons.cache:get(consumer_cache_key, nil,
-                                                      load_consumer_into_memory,
-                                                      oidcConfig.anonymous, true)
+      local consumer_cache_key = kong.db.consumers:cache_key(oidcConfig.anonymous)
+      local consumer, err      = kong.cache:get(consumer_cache_key, nil,
+                                                load_consumer_into_memory,
+                                                oidcConfig.anonymous, true)
       if err then
-        return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+        return internal_server_error(err)
       end
+
       set_consumer(consumer, nil, nil)
 
     else
-      utils.exit(500, err, ngx.HTTP_INTERNAL_SERVER_ERROR)
+      return kong.response.exit(err.status, err.message, err.headers)
     end
   end
   return res
@@ -93,19 +149,20 @@ function introspect(oidcConfig)
     if err then
       if oidcConfig.bearer_only == "yes" then
         ngx.header["WWW-Authenticate"] = 'Bearer realm="' .. oidcConfig.realm .. '",error="' .. err .. '"'
-        if oidcConfig.anonymous ~= "" then
+        if oidcConfig.anonymous then
           -- get anonymous user
-          local consumer_cache_key = singletons.db.consumers:cache_key(oidcConfig.anonymous)
-          local consumer, err      = singletons.cache:get(consumer_cache_key, nil,
-                                                          load_consumer_into_memory,
-                                                          oidcConfig.anonymous, true)
+          local consumer_cache_key = kong.db.consumers:cache_key(oidcConfig.anonymous)
+          local consumer, err      = kong.cache:get(consumer_cache_key, nil,
+                                                    load_consumer_into_memory,
+                                                    oidcConfig.anonymous, true)
           if err then
-            return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+            return internal_server_error(err)
           end
+    
           set_consumer(consumer, nil, nil)
     
         else
-          utils.exit(ngx.HTTP_UNAUTHORIZED, err, ngx.HTTP_UNAUTHORIZED)
+          return kong.response.exit(err.status, err.message, err.headers)
         end
         
       end
@@ -118,22 +175,6 @@ function introspect(oidcConfig)
 end
 
 -- TESTING
-
-local function set_consumer(consumer, credential, token)
-  ngx_set_header(constants.HEADERS.CONSUMER_ID, consumer.id)
-  ngx_set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, consumer.custom_id)
-  ngx_set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
-  ngx.ctx.authenticated_consumer = consumer
-  if credential then
-    ngx_set_header("x-authenticated-scope", token.scope)
-    ngx_set_header("x-authenticated-userid", token.authenticated_userid)
-    ngx.ctx.authenticated_credential = credential
-    ngx_set_header(constants.HEADERS.ANONYMOUS, nil) -- in case of auth plugins concatenation
-  else
-    ngx_set_header(constants.HEADERS.ANONYMOUS, true)
-  end
-
-end
 
 
 return OidcHandler
